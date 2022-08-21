6ddccb88205ff83e3232c6c666bfcd75d695f5b4
jmsjs <jmsong8@gmail.com>
2022-08-20 23:32:21 -0700

    done 01+02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringTimes = "";
+    for (i = 0; i < num; i++) {
+        stringTimes = stringTimes + string;
+    }
+    return stringTimes;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (i = 0; i < string.length(); i++) {
+        
+    }
 };
 
 // Do not edit below this line
