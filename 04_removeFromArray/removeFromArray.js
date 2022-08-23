d7f569ccd1b42260d1d957de9821618b5cda7324
jmsjs <jmsong8@gmail.com>
2022-08-22 22:56:40 -0700

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (array, ...thingsToRemove) {
+//     const result = [];
+//     array.forEach(thingInArray => {
+//         if (!thingsToRemove.includes(thingInArray)) {
+//         result.push(thingInArray);
+//       }
+//     });
+//     return result;
+//   };
+//
+// 
+//////////////////////////////
+// Trying out Filter array method
+//////////////////////////////
+    return array.filter(thingInArray => !thingsToRemove.includes(thingInArray)); 
+}
 // Do not edit below this line
 module.exports = removeFromArray;
