db491dab328bfb71ae9dfcbca5d056ccb926bed2
jmsjs <jmsong8@gmail.com>
2022-08-22 19:31:01 -0700

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,11 @@
 const reverseString = function(string) {
     let reversedString = "";
-    for (i = 0; i < string.length(); i++) {
-        
+    for (i = string.length; i >= 0; i--) {
+        let letter = string.charAt(i);
+        reversedString = reversedString + letter;
+    
     }
+    return reversedString;
 };
 
 // Do not edit below this line
