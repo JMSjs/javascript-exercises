d73bb9ad89d5694a44884a3ab87746920901aacd
jmsjs <jmsong8@gmail.com>
2022-10-06 23:00:06 -0700

    done 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+   return arr.reduce((prevObj, currObj) => { 
+        let prevObjAge = getAge(prevObj.yearOfBirth, prevObj.yearOfDeath);
+        let currObjAge = getAge(currObj.yearOfBirth, currObj.yearOfDeath);
+        console.log(prevObjAge + "vs" + currObjAge);
+        return prevObjAge < currObjAge 
+            ? currObj
+            : prevObj;
+    })
 };
+    
+function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
